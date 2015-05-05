8340754a96d4937f599bca1c76414fc9958fa3c5
Antonio <ragnell93@gmail.com>
2015-05-04 21:44:07 -0430

    prueba de creacion de la clase BilleteraElectronic

--- Tarea 3 CI-3715/BilleteraElectronica.py
+++ Tarea 3 CI-3715/BilleteraElectronica.py
@@ -1,44 +1,11 @@
-'''
-Created on May 4, 2015
-
-@author: antonio
-'''
 from collections import namedtuple
 
 class BilleteraElectronica(object):
-    
-    Consumo = namedtuple('Consumo', 'monto fecha idEst') #Estructura del consumo 
-    Credito = namedtuple('Credito', 'monto fecha idEst') #Estructura del crédito 
-    
-    listaConsumos = [] 
-    listaCreditos = []
 
-    def __init__(self, nombres, apellidos, CI, PIN):
+    def __init__(self,ident, nombres, apellidos, CI, PIN):
+        self.identificador = ident
         self.nombre = nombres
         self.apellido = apellidos
         self.CI = CI
         self.PIN = PIN
         self.saldoActual = 0
-     
-    def saldo(self):
-        return self.saldoActual    
-        
-    def recargar(self,mont,fech,id_):
-        self.saldoActual += mont
-        infoRecarga = self.Recarga(mont,fech,id_)
-        self.listaCreditos.append(infoRecarga)
-        
-    def consumir(self,mont,fech,id_,pinUsuario):
-        if self.saldoActual < mont:
-            raise Exception("No hay suficiente saldo para realizar la operacion")
-        elif pinUsuario != self.PIN:
-            raise Exception("El PIN introducido es invalido")
-        else:
-            infoConsumo = self.Consumo(mont,fech,id_)
-            self.listaConsumos.append(infoConsumo)
-            self.saldoActual -= mont
-        
-    if __name__ == '__main__':
-        pass
-    
-        
---
+++ Tarea 3 CI-3715/pruebaClase.py
@@ -0,0 +1,16 @@
+import unittest
+from collections import namedtuple
+from BilleteraElectronica import BilleteraElectronica
+
+class BilleteraElectronicaTester(unittest.TestCase):
+
+    #CASO INTERNO: probar que se crea correctamente una instancia de la clase
+    #Resultado esperado: TRUE
+
+    def testClase(self):
+        Billete = BilleteraElectronica("Dolar300","Hugo","Perez",1,1)
+
+    #resultado obtenido: No existe el mòdulo llamado "BilleteraElectronica"
+
+if __name__ == '__main__':
+    unittest.main()
